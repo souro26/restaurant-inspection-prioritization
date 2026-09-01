4bb5587087e069b99152c601e12aae91bcde2a00
Souradeep Roy <souro26.ju@gmail.com>
2026-09-01 14:14:50 +0530

    up to date

--- sql/transformations/07_build_labels.sql
+++ sql/transformations/07_build_labels.sql
@@ -1,27 +1,60 @@
 -- 07_build_labels.sql
 -- Purpose:
---     Attach the observed target outcome to each retrospective
---     restaurant decision snapshot.
+--     Attach the observed outcome of the target inspection to
+--     each retrospective restaurant decision snapshot.
 
 CREATE OR REPLACE TABLE processed.labels AS
 SELECT
     p.camis,
+
     p.cutoff_inspection_id,
     p.cutoff_date,
     p.cutoff_inspection_type,
     p.cutoff_is_cycle_initial,
     p.cutoff_is_cycle_reinspection,
+
     p.target_inspection_id,
     p.target_inspection_date,
     p.target_inspection_type,
     p.days_to_target,
-    CAST(i.has_critical_violation AS INTEGER) AS target_critical,
+
+    i.total_violations AS target_total_violations,
+    i.critical_violation_count AS target_critical_violation_count,
+    i.noncritical_violation_count AS target_noncritical_violation_count,
+    i.score AS target_score,
+    i.grade AS target_grade,
+    i.action AS target_action,
+
+    CAST(
+        i.critical_violation_count >= 1
+        AS INTEGER
+    ) AS target_any_critical,
+    CAST(
+        i.critical_violation_count >= 2
+        AS INTEGER
+    ) AS target_two_or_more_critical,
+    CAST(
+        i.critical_violation_count >= 3
+        AS INTEGER
+    ) AS target_three_or_more_critical,
+    CAST(
+        i.critical_violation_count >= 4
+        AS INTEGER
+    ) AS target_four_or_more_critical,
+    CAST(
+        i.critical_violation_count >= 3
+        AS INTEGER
+    ) AS target_high_severity,
+
     CASE
-        WHEN i.has_critical_violation THEN 'critical_outcome'
-        ELSE 'no_critical_outcome'
+        WHEN i.critical_violation_count >= 3
+            THEN 'high_severity_critical_outcome'
+        ELSE 'below_high_severity_threshold'
     END AS target_label
-FROM processed.prediction_population p
-JOIN processed.inspections i
+
+FROM processed.prediction_population AS p
+INNER JOIN processed.inspections AS i
     ON i.inspection_id = p.target_inspection_id
+
 WHERE i.is_eligible_target_event = TRUE
   AND p.target_inspection_date > p.cutoff_date;
